43e5f372b24ef2f144f05ecb1fa72305d2fe751e
Luca De Menego <luca.demenego@studenti.unitn.it>
2021-08-14 09:19:17 +0200

    Fix

--- android/src/main/kotlin/com/difrancescogianmarco/arcore_flutter_plugin/ArCoreFaceView.kt
+++ android/src/main/kotlin/com/difrancescogianmarco/arcore_flutter_plugin/ArCoreFaceView.kt
@@ -71,7 +71,7 @@ class ArCoreFaceView(activity:Activity,context: Context, messenger: BinaryMessen
 
                     val list = faceNodeMap.toList().map { it.first }
                     if (list.size > 0) {
-                        val dest = floatArrayOf(0.0)
+                        val dest = floatArrayOf()
                         list[0].getCenterPose().toMatrix(dest, 0);
                         methodChannel2.invokeMethod("onGetFacesNodes", dest)
                     }
